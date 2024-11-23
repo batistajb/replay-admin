4b3cc9eb9fb4a60f2d2722bf4cda0a65de95c3d9
joao <batistadasilvaalvesjoao@gmail.com>
2024-11-23 13:10:44 -0300

    feat: set update kub config based branch environment

--- start.sh
+++ start.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+echo "Install requirements"
 pipenv install
-pipenv shell
+
+echo "Activate virtual environment"
+source $(pipenv --venv)/bin/activate
+
+echo "Make migrations and migrate"
 python manage.py makemigrations
 python manage.py migrate
+
+echo "Run server"
 python manage.py runserver 0.0.0.0:8000
